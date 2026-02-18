29c8112387b78f81420dcfa57524c656199444bc
Anastasia <kovalchuk0687@gmail.com>
2026-02-18 15:35:12 +0100

    Added routers

--- frontend/src/components/PageWrapper.tsx
+++ frontend/src/components/PageWrapper.tsx
@@ -1,15 +1,16 @@
 import style from "./PageWrapper.module.scss";
 import Header from "./Header/Header";
-import Home from "./Main/HomePage";
+// import Home from "./Main/HomePage";
 import { ModalProvider } from "./ModalContext";
 import { ModalWindow } from "./ModalWindow";
+import { Outlet } from "react-router";
 
 export const PageWrapper = () => {
   return (
     <div className={style.container}>
       <ModalProvider>
         <Header />
-        <Home />
+        <Outlet />
         <ModalWindow />
       </ModalProvider>
     </div>
